465828fad77a4dad418e7148564de78617a17da0
swapnilsudrik <113012667+swapnilsudrik@users.noreply.github.com>
2024-03-10 19:39:04 +0530

    update backend

---
+++ backend/src/main/java/com/main/OfficeManagementSystem/OfficeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.main.OfficeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/main/OfficeManagementSystem/OfficeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.OfficeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
